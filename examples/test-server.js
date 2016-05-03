1cb77f312dce7c1c6afeb360c52a84ce0eb1d466
Ali Mihandoost <i@ali.md>
2016-05-04 03:01:23 +0430

    build: tsc

--- examples/test-server.js
+++ examples/test-server.js
@@ -1,3 +1,6 @@
+/// <reference path="../typings/main.d.ts" />
 "use strict";
 const test = require("../");
+const log = require('debug')('test-server');
+log('Test server run.');
 console.log(test.msg);
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-var a = 1;
-console.log(1);
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./src"));
