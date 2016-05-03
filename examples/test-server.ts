877ab3421cae5cf7462c38a6106ebdffd1a5cdb2
Ali Mihandoost <i@ali.md>
2016-05-03 17:57:55 +0430

    refactor: add simple module structure

---
+++ examples/test-server.ts
@@ -0,0 +1,3 @@
+import * from "../";
+
+console.log(msg);
--- index.ts
+++ index.ts
@@ -1,13 +1,1 @@
-async function test() {
-  return new Promise(function (resolve){
-    setTimeout(resolve, 2000);
-  });
-}
-
-async function test2() {
-  console.log(1);
-  await test();
-  console.log(2);
-}
-
-test2();
+export * from "./src";
