8425fdacc1b0a3d4b8326b65ece1074e4a4fa40a
Ali Mihandoost <i@ali.md>
2016-05-05 01:15:21 +0430

    test(index): setup test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,3 +1,47 @@
 /// <reference path="../typings/main.d.ts" />
 
+import TelegramBot from "./";
+
 const expect = require('expect.js');
+
+var expectToBePromise = (obj) => {
+  expect(bot).to.be.an('object');
+  expect(bot).to.have('then');
+  expect(bot.then).to.be.a('function');
+};
+
+const token = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
+
+describe('telegram bot client general', () => {
+
+  describe('setup', () => {
+
+    it.only('should be an constructor', () => {
+      expect(TelegramBot).to.be.a('function');
+      expect(new TelegramBot()).to.be.an('object');
+    });
+
+    it('should be return a promise', (done) => {
+      const bot = new TelegramBot(token);
+      expect(bot).to.be.an('object');
+      expect(bot).to.have('then');
+      expect(bot.then).to.be.a('function');
+      bot.then(done, (err) => {
+        throw err;
+      });
+    });
+
+  });
+
+  describe('api', () => {
+
+    before((done) => {
+      const bot = new TelegramBot(token);
+      bot.then(done);
+    });
+
+    it('should be worked!');
+
+  });
+
+});
