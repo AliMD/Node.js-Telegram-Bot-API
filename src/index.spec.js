eb6461beda5d860cb9a42265914c6dd29360dd93
Ali Mihandoost <i@ali.md>
2016-05-05 03:32:14 +0430

    test(bot): add basic test

--- src/index.spec.js
+++ src/index.spec.js
@@ -11,33 +11,47 @@ var expectToBePromise = (obj) => {
 };
 
 const token = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
+const defOptions = {
+  webhook: false,
+  autoUpdate: true,
+  updateInterval: 1000
+};
 
 describe('telegram bot client general', () => {
 
   describe('setup', () => {
 
-    it.only('should be an constructor', () => {
+    it('should be an constructor', () => {
       expect(TelegramBot).to.be.a('function');
       expect(new TelegramBot()).to.be.an('object');
     });
 
-    it('should be return a promise', (done) => {
-      const bot = new TelegramBot(token);
-      expect(bot).to.be.an('object');
-      expect(bot).to.have('then');
-      expect(bot.then).to.be.a('function');
-      bot.then(done, (err) => {
-        throw err;
-      });
+    it('should be have token', () => {
+      let bot = new TelegramBot(token);
+      expect(bot.token).to.be.equal(token);
+    });
+
+    it('should be have not token', () => {
+      let bot = new TelegramBot();
+      expect(bot.token).to.be.empty();
+    });
+
+    it('should be have default options', () => {
+      let bot = new TelegramBot(token);
+      expect(bot.options).to.be.eql(defOptions);
+    });
+
+    it('should be have extended options', () => {
+      let bot = new TelegramBot(token, {foo: 'bar'});
+      expect(bot.options.foo).to.be.equal('bar')
     });
 
   });
 
   describe('api', () => {
-
-    before((done) => {
-      const bot = new TelegramBot(token);
-      bot.then(done);
+    var bot;
+    before(() => {
+      bot = new TelegramBot(token);
     });
 
     it('should be worked!');
