8fd4175db8a54f104cb4def4cc0ac28830d624f7
Ali Mihandoost <i@ali.md>
2016-05-05 03:31:58 +0430

    feat(bot): add basic class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export var msg = "hi";
+const log = require('debug')('TelegramBot index');
+const _ = require('lodash/core');
+
+/**
+ * @class TelegramBot
+ * @param {string} tocken
+ */
+export default class TelegramBot {
+  public options = {
+    webhook: false,
+    autoUpdate: true,
+    updateInterval: 1000
+  }
+
+  constructor (public token:string = '', opt:Object = {}) {
+    _.extend(this.options, opt);
+  }
+}
