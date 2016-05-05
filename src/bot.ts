68226591dc41c88e3d3c3a079b85f6ebfa185028
Ali Mihandoost <i@ali.md>
2016-05-05 22:40:52 +0430

    feat(bot): add makeUrl

--- src/bot.ts
+++ src/bot.ts
@@ -9,25 +9,31 @@ const log = debug('TelegramBot index');
  * @param {string} tocken
  */
 
+const BASE_API_URL: string = 'https://api.telegram.org/';
+
 export default class TelegramBot {
-  public options = {
+  public options: Object = {
     webhook: false,
     autoUpdate: true,
     updateInterval: 1000
   }
 
-  constructor (public token: string = '', public opt: {
+  constructor (public token: string = '', opt: {
       webhook: boolean,
       autoUpdate: boolean,
       updateInterval: number
     }) {
-
+    log('new TelegramBot');
     console.assert(typeof token === 'string', 'token must be string');
     _.extend(this.options, opt);
   }
 
+  makeUrl (methodName: string) {
+    return `${BASE_API_URL}bot${this.token}/${methodName}`;
+  }
+
   async query (methodName: string, parameters: Object) {
-    return 1;
+    return;
   }
 
 }
