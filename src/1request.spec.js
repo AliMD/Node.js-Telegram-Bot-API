152f0b23a6e733d9e5ae841c74d298406e226ed3
Ali Mihandoost <i@ali.md>
2016-05-06 01:49:22 +0430

    test(1request): create server for test

--- src/1request.spec.js
+++ src/1request.spec.js
@@ -9,7 +9,40 @@ function expectToBePromise (obj) {
   expect(obj).to.have.property('then');
   expect(obj.then).to.be.a('function');
 };
+
+const
+host = '127.0.0.1',
+port = 3010,
+serverUrl = `http://${host}:${port}/`
+;
+
+var server = http.createServer((req, resp) => {
+  try {
+    if (req.url === '/') {
+      resp.writeHead(200, {'Content-Type': 'text/plain'});
+      resp.end('okay');
+    }
+    else {
+      resp.writeHead(200, {'Content-Type': 'text/plain'});
+      resp.end(req.url.substr(1));
+    }
+  }
+  catch (err) {
+    console.error(err);
+  }
+});
+
 describe('_1request', () => {
+  before ((done) => {
+    console.log(`server listen on ${port}`);
+    server.listen(port, host, null, done);
+  });
+
+  after ((done) => {
+    console.log('server close');
+    server.close(done);
+  })
+
   it('shoud return promise', () => {
     expectToBePromise(_1request({}));
   });
