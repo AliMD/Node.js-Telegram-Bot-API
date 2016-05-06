4ae0467e2fd7736dd3906dddabaa437607bc74cb
Ali Mihandoost <i@ali.md>
2016-05-07 00:57:04 +0430

    test(events): test all events structure

--- src/telegram-bot-events.spec.js
+++ src/telegram-bot-events.spec.js
@@ -7,7 +7,6 @@ const
 token = process.env.TEST_TOCKEN || '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11',
 userId = process.env.TEST_USERID || 777000,
 defOptions = {
-  webhook: false,
   autoUpdate: true,
   updateInterval: 1000
 },
@@ -21,6 +20,9 @@ expectToBePromise = (obj) => {
 
 describe('bot.js', () => {
   var bot = new TelegramBotApi(token, defOptions);
+  after(() => {
+    bot.stopAutoUpdates();
+  })
 
   describe('new instance', () => {
 
@@ -86,7 +88,7 @@ describe('bot.js', () => {
   });
 
   describe.only('events', () => {
-    after(() => {
+    afterEach(() => {
       bot.offAll();
     });
 
@@ -115,6 +117,93 @@ describe('bot.js', () => {
       expect(bot.events.on).to.be.a('function');
     });
 
+    it('should can add listner', (done) => {
+      let callback = function () {
+        done('callback called !')
+      };
+
+      bot.on('junk', callback);
+      let listeners = bot.events.listeners('junk');
+
+      expect(listeners).to.be.an('array');
+      expect(listeners.length).to.be.equal(1);
+      expect(listeners[0]).to.be.equal(callback);
+      done();
+    });
+
+    it('should can remove all special listners', (done) => {
+      let callback1 = function () {
+        done('callback1 called !')
+      };
+      bot.on('junk', callback1);
+      bot.on('junk', callback1);
+
+      let callback2 = function () {
+        done('callback2 called !')
+      };
+      bot.on('junk', callback2);
+      bot.on('junk', callback2);
+
+      let callback3 = function () {
+        done('callback3 called !')
+      };
+      bot.on('junk2', callback2);
+
+      bot.offAll('junk');
+
+      expect(bot.events.listeners('junk').length).to.be.equal(0);
+      expect(bot.events.listeners('junk2').length).to.be.equal(1);
+      done();
+    });
+
+    it('should can remove all listners', (done) => {
+      let callback1 = function () {
+        done('callback1 called !')
+      };
+      bot.on('junk', callback1);
+      bot.on('junk', callback1);
+
+      let callback2 = function () {
+        done('callback2 called !')
+      };
+      bot.on('junk', callback2);
+      bot.on('junk', callback2);
+
+      let callback3 = function () {
+        done('callback3 called !')
+      };
+      bot.on('junk2', callback2);
+
+      bot.offAll();
+
+      expect(bot.events.listeners('junk').length).to.be.equal(0);
+      expect(bot.events.listeners('junk2').length).to.be.equal(0);
+      done();
+    });
+
+    it('should can remove a special listner', (done) => {
+      let callback1 = function () {
+        done('callback1 called !')
+      };
+      bot.on('junk', callback1);
+
+      let callback2 = function () {
+        done('callback2 called !')
+      };
+      bot.on('junk', callback2);
+
+      expect(bot.events.listeners('junk').length).to.be.equal(2); // also check offAll in afterEach
+
+      bot.off('junk', callback1);
+
+      expect(bot.events.listeners('junk').length).to.be.equal(1);
+      expect(bot.events.listeners('junk')[0]).to.be.equal(callback2);
+    });
+
+
+  })
+
+  describe('auto update', () => {
     it('should trigger on mesage', (done) => {
       console.log('send message to this bot for test');
       bot.on('message', () => {
