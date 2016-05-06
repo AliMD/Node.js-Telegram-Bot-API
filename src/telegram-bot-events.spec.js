a9fd3106591c3f7b30e3947f3c70cb0b04df190d
Ali Mihandoost <i@ali.md>
2016-05-06 20:00:23 +0430

    fix(event): fix import in spec file

--- src/telegram-bot-events.spec.js
+++ src/telegram-bot-events.spec.js
@@ -1,7 +1,7 @@
 import "babel-polyfill";
 import expect from 'expect.js';
 
-import TelegramBotApi from '../';
+import TelegramBotApi from './telegram-bot-events';
 
 const
 token = process.env.TEST_TOCKEN || '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11',
