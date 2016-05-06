4a6c04c1caa1612ce5b64e3f683287eedd1f5d16
Ali Mihandoost <i@ali.md>
2016-05-06 04:46:08 +0430

    doc(bot): add doc for all methods

--- src/bot.d.ts
+++ src/bot.d.ts
@@ -2,33 +2,181 @@
 export default class TelegramBot {
     token: string;
     options: Object;
-    constructor(token: string, opt: {
+    /**
+     * create a TelegramBot
+     * @param {string} token
+     * @param {Object} options
+     */
+    constructor(token: string, options: {
         webhook: boolean;
         autoUpdate: boolean;
         updateInterval: number;
     });
+    /**
+     * Make telegram api query url
+     * @param {string} methodName
+     * @returns {string} url
+     */
     makeUrl(methodName: string): string;
+    /**
+     * Send query to telegram api server
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     query(methodName: string, parameters?: Object): Promise<{}>;
+    /**
+     * Send query for getMe
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     getMe(): Promise<{}>;
+    /**
+     * Send query for sendMessage
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendMessage(parameters: Object): Promise<{}>;
+    /**
+     * Send query for forwardMessage
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     forwardMessage(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendPhoto
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendPhoto(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendAudio
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendAudio(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendDocument
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendDocument(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendSticker
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendSticker(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendVideo
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendVideo(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendVoice
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendVoice(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendLocation
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendLocation(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendVenue
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendVenue(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendContact
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendContact(parameters: Object): Promise<{}>;
+    /**
+     * Send query for sendChatAction
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     sendChatAction(parameters: Object): Promise<{}>;
+    /**
+     * Send query for getUserProfilePhotos
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     getUserProfilePhotos(parameters: Object): Promise<{}>;
+    /**
+     * Send query for getFile
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     getFile(parameters: Object): Promise<{}>;
+    /**
+     * Send query for kickChatMember
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     kickChatMember(parameters: Object): Promise<{}>;
+    /**
+     * Send query for unbanChatMember
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     unbanChatMember(parameters: Object): Promise<{}>;
+    /**
+     * Send query for answerCallbackQuery
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     answerCallbackQuery(parameters: Object): Promise<{}>;
+    /**
+     * Send query for editMessageText
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     editMessageText(parameters: Object): Promise<{}>;
+    /**
+     * Send query for editMessageCaption
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     editMessageCaption(parameters: Object): Promise<{}>;
+    /**
+     * Send query for editMessageReplyMarkup
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     editMessageReplyMarkup(parameters: Object): Promise<{}>;
+    /**
+     * Send query for answerInlineQuery
+     * @param  {string} methodName
+     * @param  {Object} parameters
+     * @returns {Promise} requet promise
+     */
     answerInlineQuery(parameters: Object): Promise<{}>;
 }
--- src/bot.ts
+++ src/bot.ts
@@ -20,20 +20,36 @@ export default class TelegramBot {
     updateInterval: 1000
   }
 
-  constructor(public token: string = '', opt: {
+  /**
+   * create a TelegramBot
+   * @param {string} token
+   * @param {Object} options
+   */
+  constructor(public token: string = '', options: {
     webhook: boolean,
     autoUpdate: boolean,
     updateInterval: number
   }) {
     log('new TelegramBot');
     console.assert(typeof token === 'string', 'token must be string');
-    _extend(this.options, opt);
+    _extend(this.options, options);
   }
 
+  /**
+   * Make telegram api query url
+   * @param {string} methodName
+   * @returns {string} url
+   */
   makeUrl(methodName: string) {
     return `${BASE_API_URL}bot${this.token}/${methodName}`;
   }
 
+  /**
+   * Send query to telegram api server
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async query(methodName: string, parameters?: Object): Promise<{}> {
     let requestOptions = {
       url: this.makeUrl(methodName),
@@ -47,90 +63,222 @@ export default class TelegramBot {
       });
   }
 
+  /**
+   * Send query for getMe
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async getMe(): Promise<{}> {
     return this.query('sendMessage');
   }
 
+  /**
+   * Send query for sendMessage
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendMessage(parameters: Object): Promise<{}> {
     return this.query('sendMessage', parameters);
   }
 
+  /**
+   * Send query for forwardMessage
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async forwardMessage(parameters: Object): Promise<{}> {
     return this.query('forwardMessage', parameters);
   }
 
+  /**
+   * Send query for sendPhoto
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendPhoto(parameters: Object): Promise<{}> {
     return this.query('sendPhoto', parameters);
   }
 
+  /**
+   * Send query for sendAudio
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendAudio(parameters: Object): Promise<{}> {
     return this.query('sendAudio', parameters);
   }
 
+  /**
+   * Send query for sendDocument
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendDocument(parameters: Object): Promise<{}> {
     return this.query('sendDocument', parameters);
   }
 
+  /**
+   * Send query for sendSticker
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendSticker(parameters: Object): Promise<{}> {
     return this.query('sendSticker', parameters);
   }
 
+  /**
+   * Send query for sendVideo
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendVideo(parameters: Object): Promise<{}> {
     return this.query('sendVideo', parameters);
   }
 
+  /**
+   * Send query for sendVoice
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendVoice(parameters: Object): Promise<{}> {
     return this.query('sendVoice', parameters);
   }
 
+  /**
+   * Send query for sendLocation
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendLocation(parameters: Object): Promise<{}> {
     return this.query('sendLocation', parameters);
   }
 
+  /**
+   * Send query for sendVenue
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendVenue(parameters: Object): Promise<{}> {
     return this.query('sendVenue', parameters);
   }
 
+  /**
+   * Send query for sendContact
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendContact(parameters: Object): Promise<{}> {
     return this.query('sendContact', parameters);
   }
 
+  /**
+   * Send query for sendChatAction
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async sendChatAction(parameters: Object): Promise<{}> {
     return this.query('sendChatAction', parameters);
   }
 
+  /**
+   * Send query for getUserProfilePhotos
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async getUserProfilePhotos(parameters: Object): Promise<{}> {
     return this.query('getUserProfilePhotos', parameters);
   }
 
+  /**
+   * Send query for getFile
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async getFile(parameters: Object): Promise<{}> {
     return this.query('getFile', parameters);
   }
 
+  /**
+   * Send query for kickChatMember
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async kickChatMember(parameters: Object): Promise<{}> {
     return this.query('kickChatMember', parameters);
   }
 
+  /**
+   * Send query for unbanChatMember
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async unbanChatMember(parameters: Object): Promise<{}> {
     return this.query('unbanChatMember', parameters);
   }
 
+  /**
+   * Send query for answerCallbackQuery
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async answerCallbackQuery(parameters: Object): Promise<{}> {
     return this.query('answerCallbackQuery', parameters);
   }
 
+  /**
+   * Send query for editMessageText
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async editMessageText(parameters: Object): Promise<{}> {
     return this.query('editMessageText', parameters);
   }
 
+  /**
+   * Send query for editMessageCaption
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async editMessageCaption(parameters: Object): Promise<{}> {
     return this.query('editMessageCaption', parameters);
   }
 
+  /**
+   * Send query for editMessageReplyMarkup
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async editMessageReplyMarkup(parameters: Object): Promise<{}> {
     return this.query('editMessageReplyMarkup', parameters);
   }
 
+  /**
+   * Send query for answerInlineQuery
+   * @param  {string} methodName
+   * @param  {Object} parameters
+   * @returns {Promise} requet promise
+   */
   async answerInlineQuery(parameters: Object): Promise<{}> {
     return this.query('answerInlineQuery', parameters);
   }
