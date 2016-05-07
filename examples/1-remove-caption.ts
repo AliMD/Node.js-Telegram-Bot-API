e6427135ffb6691b0ddc9e56e74fae1cb5d70237
Ali Mihandoost <i@ali.md>
2016-05-07 23:45:04 +0430

    chore(example): use test token

--- examples/1-remove-caption.ts
+++ examples/1-remove-caption.ts
@@ -2,7 +2,7 @@
 
 import TelegramBot from '../index';
 
-const token = process.env.BOT_TOCKEN || '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+const token = process.env.TEST_TOKEN || '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 const bot = new TelegramBot(token, {autoUpdate: true});
 
 bot.on('update.message.photo', (msg) => {
