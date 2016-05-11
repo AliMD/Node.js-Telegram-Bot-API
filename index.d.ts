94c337f8cea69640059f3ad3949a3ebfbe893d1f
Ali Mihandoost <i@ali.md>
2016-05-11 22:06:06 +0430

    release: v0.6.7

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="typings/main.d.ts" />
 /**
  * Telegram Bot API for Node.js with ESNext
  * @version 0.0.1
--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  * @license MIT
  */
 "use strict";
-/// <reference path="typings/main.d.ts" />
 const telegram_bot_events_1 = require('./src/telegram-bot-events');
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = telegram_bot_events_1.default;
