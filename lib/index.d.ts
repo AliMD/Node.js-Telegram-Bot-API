eda5f6a92033c9ed20a14bc0585d90fd15d92c27
Ali Mihandoost <i@ali.md>
2016-08-25 14:56:11 +0430

    build: tsc

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Telegram Bot API for Node.js with ESNext
+ * @version 0.0.1
+ * @author Ali Mihandoost <i@ali.md> (http://ali.md/)
+ * @license MIT
+ */
+import TelegramBotApi from './telegram-bot-events';
+export default TelegramBotApi;
